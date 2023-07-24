36e22dae4f4e91c57609a0789b1de1cf7f149a99
Samin Chowdhury <samin@Samins-Mac-mini.local>
2023-07-24 11:19:14 +0100

    added register page

--- client/src/components/RegisterForm/index.jsx
+++ client/src/components/RegisterForm/index.jsx
@@ -1,5 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function RegisterForm() {
-  return <div></div>;
+
+  const [firstName, setFirstName] = useState('')
+  const [lastName, setLastName] = useState('')
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+  function handleFirstName(e) {
+    setFirstName(e.target.value)
+  }
+
+  function handleLastName(e) {
+    setLastName(e.target.value)
+  }
+
+  function handleUsername(e) {
+    setUsername(e.target.value)
+  }
+
+  function handlePassword(e) {
+    setPassword(e.target.value)
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>First Name: <input type="text" value={firstName} onChange={handleFirstName} /></label>
+      <br />
+      <label>Last Name: <input type="text" value={lastName} onChange={handleLastName} /></label>
+      <br />
+      <label>Username: <input type="text" value={username} onChange={handleUsername} /></label>
+      <br />
+      <label>Password: <input type="password" value={password} onChange={handlePassword} /></label>
+      <br />
+      <input type="submit" value="Register" />
+    </form>
+  )
 }
--- client/src/pages/RegisterPage/index.jsx
+++ client/src/pages/RegisterPage/index.jsx
@@ -1,5 +1,11 @@
 import React from "react";
+import { RegisterForm } from "../../components";
 
 export default function RegisterPage() {
-  return <h1>Please Register</h1>;
+  return (
+    <div>
+      <h1>Please Register</h1>
+      <RegisterForm />
+    </div>
+  )
 }
