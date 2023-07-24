b3948490e7fc9e02c57709c879831104df62c974
Samin Chowdhury <samin@Samins-Mac-mini.local>
2023-07-24 10:46:10 +0100

    created login page

--- client/src/components/LoginForm/index.jsx
+++ client/src/components/LoginForm/index.jsx
@@ -1,5 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function LoginForm() {
-  return <div></div>;
+
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+  function handleUsername(e) {
+    setUsername(e.target.value)
+  }
+
+  function handlePassword(e) {
+    setPassword(e.target.value)
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  }
+
+  return(
+    <form onSubmit={handleSubmit}>
+      <label>Username:<input type="text" value={username} onChange={handleUsername} /></label>
+      <br />
+      <label>Password:<input type="password" value={password} onChange={handlePassword} /></label>
+      <br />
+      <input type="submit" value="Submit" />
+    </form>
+  )
 }
