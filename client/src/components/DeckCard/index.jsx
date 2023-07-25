4fc8ade4ac8c76146d0c993b2cb86dd5a330946c
Samin Chowdhury <samin@Samins-Mac-mini.local>
2023-07-25 14:28:29 +0100

    added DeckPage

--- client/src/components/DeckCard/index.jsx
+++ client/src/components/DeckCard/index.jsx
@@ -1,21 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const linkStyle = {
-    color: '#875053'
-};
-
-const DeckCard = ({ id, name, subject, tags, likes, image}) => {
-    return (
-        <div className='deck-card'>
-            <h3><Link to={`/decks/${id}`} style={linkStyle}>{name}</Link></h3>
-            <p>Subject: {subject}</p>
-            <p>Tags: {tags.join(', ')}</p>
-            <p>Likes: {likes}</p>
-            {image && <img src={image} alt={name} />}
-        </div>
-    )
-};
+const DeckCard = ({ id, name, subject, tags, likes, image }) => {
+  return (
+    <div className='deck-card'>
+      <Link to={`/decks/${id}`}>
+        <h3>{name}</h3>
+        <img src={image} alt={name} />
+        <p>Subject: {subject}</p>
+        <p>Tags: {tags.join(', ')}</p>
+        <p>Likes: {likes}</p>
+      </Link>
+    </div>
+  )
+}
 
 export default DeckCard;
+
 
