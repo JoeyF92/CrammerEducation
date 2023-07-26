84b7fa116c36aa92b4fe0161ecfc918a38a09489
Nastasia777 <kuznenastia@gmail.com>
2023-07-26 11:45:43 +0100

    some css

--- client/src/components/DeckCard/index.jsx
+++ client/src/components/DeckCard/index.jsx
@@ -1,76 +1,73 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
+const DeckCard = ({ id, name, subject, tags, likes, image }) => {
+  const [isLiked, setIsLiked] = useState(false);
+  const [likesCount, setLikesCount] = useState(likes);
+  const token = JSON.parse(localStorage.getItem("token"));
+  const userId = token.user_id;
 
-const DeckCard = ({ id, name, subject, tags, likes, image}) => {
+  const handleLikeUnlike = async () => {
+    try {
+      const isLikedNow = !isLiked;
+      setIsLiked(isLikedNow);
 
-    const [isLiked, setIsLiked] = useState(false);
-    const [likesCount, setLikesCount] = useState(likes)
-    const token = JSON.parse(localStorage.getItem("token"));
-    const userId = token.user_id;
-
-    const handleLikeUnlike = async () => {
-        try {
-            //toggle isLiked state
-          const isLikedNow = !isLiked;
-          setIsLiked(isLikedNow); 
-    
-          // Send requests to update likes in users and decks tables
-          const userLikePromise = fetch(`http://localhost:3000/users/${userId}/decks/${id}`, {
-            method: "PATCH",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify({ isLiked: isLikedNow }),
-          });
-    
-          const deckLikePromise = fetch(`http://localhost:3000/decks/${id}`, {
-            method: "PATCH",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify({ isLiked: isLikedNow}),
-          });
-    
-          // Wait for both requests to complete
-          const [userLikeResponse, deckLikeResponse] = await Promise.all([userLikePromise, deckLikePromise]);
-    
-          if (userLikeResponse.ok && deckLikeResponse.ok) {
-            // Update the likes count on the card
-            setLikesCount(isLikedNow ? likesCount + 1 : likesCount - 1);
-
-          } else {
-            console.error("Error liking/unliking deck.");
-          }
-        } catch (error) {
-          console.error("Error liking/unliking deck.", error);
+      const userLikePromise = fetch(
+        `http://localhost:3000/users/${userId}/decks/${id}`,
+        {
+          method: "PATCH",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ isLiked: isLikedNow }),
         }
-      };
+      );
 
+      const deckLikePromise = fetch(`http://localhost:3000/decks/${id}`, {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ isLiked: isLikedNow }),
+      });
 
-    return (
-        <main>
-        <Link to={`/decks/${id}`}>
-        <div className='card'>
-            <h3 className='deck-name'>{name}</h3>
+      const [userLikeResponse, deckLikeResponse] = await Promise.all([
+        userLikePromise,
+        deckLikePromise,
+      ]);
 
-            <p className='subject'>{subject}</p>
-            <p className='tags'># {tags.join(', ')}</p>
+      if (userLikeResponse.ok && deckLikeResponse.ok) {
+        setLikesCount(isLikedNow ? likesCount + 1 : likesCount - 1);
+      } else {
+        console.error("Error liking/unliking deck.");
+      }
+    } catch (error) {
+      console.error("Error liking/unliking deck.", error);
+    }
+  };
 
-            {image && <img src={image} alt={name} />}
+  return (
+    <main>
+      <Link to={`/decks/${id}`}>
+        <div className="card">
+          <h3 className="deck-name">{name}</h3>
+
+          <p className="subject">{subject}</p>
+          <p className="tags"># {tags.join(", ")}</p>
+
+          {image && <img src={image} alt={name} />}
         </div>
-        </Link> <br></br>
-        <div className='likes'>
-        <button onClick= {handleLikeUnlike} className='like-button'>
-        <FontAwesomeIcon icon="fa-solid fa-heart" size="lg" />
+      </Link>{" "}
+      <br></br>
+      <div className="likes">
+        <button onClick={handleLikeUnlike} className="like-button">
+          <FontAwesomeIcon icon="fa-solid fa-heart" size="lg" />
         </button>
-        {likesCount}       
-        </div>  
-        </main>
-    )
+        {likesCount}
+      </div>
+    </main>
+  );
 };
 
 export default DeckCard;
-
-
